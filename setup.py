885b8d027d90efe696797818892c590edf8b0a99
Jung Gyu Yoon <yjg30737@gmail.com>
2023-06-22 14:38:45 +0900

    rename module

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 setup(
-    name='pyqt-huggingface-model-table',
+    name='huggingface-gui',
     version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='Manage HuggingFace models with Python desktop app',
-    url='https://github.com/yjg30737/pyqt-huggingface-model-table.git',
+    url='https://github.com/yjg30737/huggingface-gui.git',
     long_description_content_type='text/markdown',
     long_description=long_description,
     install_requires=[
